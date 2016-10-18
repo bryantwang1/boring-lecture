966f10a717499021010052843cd3a74b26956944
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-18 13:01:08 -0700

    Add js and css that adds background color, border, margin, and bold text on paragraphs when they are clicked.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("#par1").click(function() {
+    $("#par1").toggleClass("box");
+  });
+  $("#par2").click(function() {
+    $("#par2").toggleClass("box");
+  });
+  $("#par3").click(function() {
+    $("#par3").toggleClass("box");
+  });
+  $("#par4").click(function() {
+    $("#par4").toggleClass("box");
+  });
+  $("#par5").click(function() {
+    $("#par5").toggleClass("box");
+  });
+  $("#par6").click(function() {
+    $("#par6").toggleClass("box");
+  });
+});
